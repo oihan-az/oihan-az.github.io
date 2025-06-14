04b4d9dc83351b38a0d68bd16f9b44e8b97d5cfb
HOME\oihan <oihan.a@digipen.edu>
2025-06-14 14:53:00 +0200

    Improved index, and created json mith projects for scalability

---
+++ projects.js
@@ -0,0 +1,54 @@
+const projectsData = {
+  "Games": {
+    "main": [
+      {
+        "title": "Zima Polaris",
+        "link": "src/zima-polaris.html",
+        "image": "images/Zima Polaris/ZimaPolaris.png",
+        "description": "2D platform, adventure, and puzzle video game."
+      },
+      {
+        "title": "Drop-Off Duty",
+        "link": "src/drop-off-duty.html",
+        "image": "images/Drop-Off Duty/Game icon-full.png",
+        "description": "3D time trial driving and shooting video game."
+      }
+    ],
+    "Others": []
+  },
+  "Graphics": {
+    "main": [
+      {
+        "title": "Side channel trace viewer",
+        "link": "src/side-channel-trace-viewer.html",
+        "image": "images/dflt.png"
+      },
+      {
+        "title": "Acceleration data structures",
+        "link": "src/acc-structs.html",
+        "image": "images/dflt.png"
+      },
+      {
+        "title": "Model inspector (Vulkan)",
+        "link": "src/model-inspector.html",
+        "image": "images/dflt.png",
+        "wip": true
+      }
+    ],
+    "Others": [
+      {
+        "title": "Drop-Off Duty",
+        "link": "src/drop-off-duty.html"
+      }
+    ]
+  },
+  "Physics": {
+    "main": [],
+    "Others": [
+      {
+        "title": "Acceleration data structures",
+        "link": "src/acc-structs.html"
+      }
+    ]
+  }
+};
